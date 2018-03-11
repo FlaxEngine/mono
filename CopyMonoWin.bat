169f57b872257c56bf66151237e31a9d64cfb972
mafiesto4 <wojtek@figat.pl>
2018-03-11 18:30:02 +0100

    Added helper scripts for mono lib deploy to flax

---
+++ CopyMonoWin.bat
@@ -0,0 +1,32 @@
+:: Testing static linking deploy
+::del /S /Q "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libgcmonosgen.*"
+::del /S /Q "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libtest.*"
+::del /S /Q "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\MonoPosixHelper.*"
+::del /S /Q "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\mono-2.0.*"
+::del /S /Q "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\mono-2.0-dac.*"
+::del /S /Q "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\*.exp"
+
+::xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\Lib\Debug\*.lib" "C:\Flax\Source\3rdParty\Windows\x64\Debug\"
+::xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\Lib\Debug\*.pdb" "C:\Flax\Source\3rdParty\Windows\x64\Debug\"
+
+:: Win64
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\mono-2.0.lib" "C:\Flax\Source\3rdParty\Windows\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\Bin\Debug\mono-2.0.dll" "C:\Flax\Source\3rdParty\Windows\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\Bin\Debug\mono-2.0.pdb" "C:\Flax\Source\3rdParty\Windows\x64\Debug\"
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\mono-2.0.lib" "C:\Flax\Source\3rdParty\Windows\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\Bin\Release\mono-2.0.dll" "C:\Flax\Source\3rdParty\Windows\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\Bin\Release\mono-2.0.pdb" "C:\Flax\Source\3rdParty\Windows\x64\Release\"
+
+:: Win32
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\Win32\lib\Debug\mono-2.0.lib" "C:\Flax\Source\3rdParty\Windows\Win32\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\Win32\Bin\Debug\mono-2.0.dll" "C:\Flax\Source\3rdParty\Windows\Win32\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\Win32\Bin\Debug\mono-2.0.pdb" "C:\Flax\Source\3rdParty\Windows\Win32\Debug\"
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\Win32\lib\Release\mono-2.0.lib" "C:\Flax\Source\3rdParty\Windows\Win32\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\Win32\Bin\Release\mono-2.0.dll" "C:\Flax\Source\3rdParty\Windows\Win32\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\Win32\Bin\Release\mono-2.0.pdb" "C:\Flax\Source\3rdParty\Windows\Win32\Release\"
+
+echo "Done!"
---
+++ CopyMonoXbox.bat
@@ -0,0 +1,20 @@
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\eglib.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libgc.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoruntime.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmono-static.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoutils.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\eglib.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libgc.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoruntime.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmono-static.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoutils.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\eglib.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libgc.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libmonoruntime.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libmono-static.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libmonoutils.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
+
+echo "Done!"
