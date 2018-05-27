d0a265e08ab697c42989cbb93bfc92d89691d722
mafiesto4 <wojtek@figat.pl>
2018-05-28 01:25:14 +0200

    Update deploy scripts

--- CopyMonoXbox.bat
+++ CopyMonoXbox.bat
@@ -1,20 +1,24 @@
 
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\eglib.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libgc.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoruntime.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmono-static.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoutils.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+:: Debug
 
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\eglib.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libgc.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoruntime.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmono-static.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Debug\libmonoutils.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\eglib.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libgcmonosgen.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libmonoruntime.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libmono-static.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libmonoutils.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
 
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\eglib.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libgc.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libmonoruntime.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libmono-static.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
-xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\boehm\x64\lib\Release\libmonoutils.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Release\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\eglib.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libgcmonosgen.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libmonoruntime.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libmono-static.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Debug\libmonoutils.pdb" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+
+:: Release
+
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Release\eglib.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Release\libgcmonosgen.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Release\libmonoruntime.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Release\libmono-static.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
+xcopy /s /Y "C:\Flax\3rdParty\mono\msvc\build\xbox\sgen\x64\lib\Release\libmonoutils.lib" "C:\Flax\Source\3rdParty\XboxOne\x64\Debug\"
 
 echo "Done!"
